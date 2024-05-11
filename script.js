3f49438735385bb0e7c7dfa274a63b7f1c118e7a
fina <finarifana22@upi.edu>
2024-05-11 18:20:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -6,21 +6,31 @@ function calculateGCD(a, b) {
     return calculateGCD(b, a % b);
 }
 
+// Fungsi untuk menangani pengiriman formulir GCD
+function handleGCDFormSubmit(event) {
+    event.preventDefault(); // Menghentikan perilaku default formulir (refresh halaman)
+    var num1 = parseInt(document.getElementById('gcdNum1').value);
+    var num2 = parseInt(document.getElementById('gcdNum2').value);
+    var gcd = calculateGCD(num1, num2);
+    document.getElementById('gcdResult').innerHTML = "GCD (PBB) of " + num1 + " and " + num2 + " is: " + gcd;
+}
+
+// Menangani pengiriman formulir GCD
+document.getElementById('gcdForm').addEventListener('submit', handleGCDFormSubmit);
+
 // Fungsi untuk menghitung LCD (KPK)
 function calculateLCD(a, b) {
     return (a * b) / calculateGCD(a, b);
 }
 
-// Fungsi untuk menangani pengiriman formulir
-function handleFormSubmit(event) {
+// Fungsi untuk menangani pengiriman formulir LCD
+function handleLCDFormSubmit(event) {
     event.preventDefault(); // Menghentikan perilaku default formulir (refresh halaman)
-    var num1 = parseInt(document.getElementById('num1').value);
-    var num2 = parseInt(document.getElementById('num2').value);
-    var gcd = calculateGCD(num1, num2);
+    var num1 = parseInt(document.getElementById('lcdNum1').value);
+    var num2 = parseInt(document.getElementById('lcdNum2').value);
     var lcd = calculateLCD(num1, num2);
-    document.getElementById('result').innerHTML = "GCD (PBB) of " + num1 + " and " + num2 + " is: " + gcd + "<br>" +
-                                                     "LCD (KPK) of " + num1 + " and " + num2 + " is: " + lcd;
+    document.getElementById('lcdResult').innerHTML = "LCD (KPK) of " + num1 + " and " + num2 + " is: " + lcd;
 }
 
-// Menangani pengiriman formulir
-document.getElementById('gcdLcdForm').addEventListener('submit', handleFormSubmit);
+// Menangani pengiriman formulir LCD
+document.getElementById('lcdForm').addEventListener('submit', handleLCDFormSubmit);
